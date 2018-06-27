5d1508e8e05e063ee344276548e07ce7559ed666
Jason Medcoff <jason.medcoff@ally.com>
2018-06-27 10:50:15 -0400

    added some commentary

--- server/src/main/java/com/ally/invoicify/api/InvoiceApiController.java
+++ server/src/main/java/com/ally/invoicify/api/InvoiceApiController.java
@@ -24,27 +24,52 @@ public class InvoiceApiController {
     this.invoiceRepo = companyRepo;
   }
 
+  /***
+   * Mapping to return all invoices.
+   * @return
+   */
   @GetMapping("")
   public List<Invoice> getAll() {
     return invoiceRepo.findAll();
   }
 
+  /***
+   * Mapping to return an invoice with given id.
+   * @param id
+   * @return
+   */
   @GetMapping("{id}")
   public Invoice getOne(@PathVariable int id) {
     return invoiceRepo.findOne(id);
   }
 
+  /***
+   * Mapping (POST) to create an invoice in the system.
+   * @param invoice
+   * @return
+   */
   @PostMapping("")
   public Invoice create(@RequestBody Invoice invoice) {
     return invoiceRepo.save(invoice);
   }
 
+  /***
+   * Mapping (PUT) to update an invoice's id.
+   * @param invoice
+   * @param id
+   * @return
+   */
   @PutMapping("{id}")
   public Invoice update(@RequestBody Invoice invoice, @PathVariable int id) {
     invoice.setId(id);
     return invoiceRepo.save(invoice);
   }
 
+  /***
+   * Mapping (DELETE) to remove an invoice with id from the system.
+   * @param id
+   * @return
+   */
   @DeleteMapping("{id}")
   public Invoice delete(@PathVariable int id) {
     Invoice actor = invoiceRepo.findOne(id);
