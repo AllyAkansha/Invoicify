6abdb1803428110e7ae7ca4c596a3a585200f2a8
Evan Bradley <evan.bradley@ally.com>
2018-06-27 10:36:10 -0400

    Fleshed out basic front-end

---
+++ client/src/app/company.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core'
+import { Http, Response } from '@angular/http'
+
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  private baseUrl: string = 'http://localhost:4200/api/company'
+
+  constructor(private http: Http) { }
+
+  getAllCompanies(): Observable<any> {
+    return this.http.get(this.baseUrl)
+      .map(result => result.json())
+  }
+
+  getCompany(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`)
+      .map(result => result.json())
+  }
+
+  createCompany(name: String): Observable<any> {
+    return this.http.post(this.baseUrl, { name })
+      .map(result => result.json())
+  }
+
+  updateCompany(id: number, name: String): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, { name })
+      .map(result => result.json())
+  }
+
+  deleteCompany(id: number,): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`)
+      .map(result => result.json())
+  }
+}
