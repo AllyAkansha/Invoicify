5dff1da2cf3a2c795b495861086d5ac6919164c5
CZBDL3@ALLYIPOC.COM <CZBDL3@ALLYIPOC.COM>
2018-06-28 10:44:17 -0400

    HTML Tables and Buttons

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CompanyComponent } from './company/company.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { InvoiceModalComponent } from './invoice-modal/invoice-modal.component';
+
+import { BillingRecordComponent } from './billing-record/billing-record.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyComponent
+    CompanyComponent,
+    InvoiceComponent,
+    InvoiceModalComponent,
+    BillingRecordComponent
   ],
   imports: [
     BrowserModule,
